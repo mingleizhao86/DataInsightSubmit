0f2397704f7af38b6ec5985a34632e23935e74f3
mingleizhao86 <41271303+mingleizhao86@users.noreply.github.com>
2018-07-17 05:27:05 -0400

    Update pharmacy_counting.py

--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -1,28 +1,17 @@
 from collections import defaultdict
 
 
-class PharmacyCount():
-    def __init__(self):
-        pass
-
-
 drug_num_cost = defaultdict(dict)
-
 sucriber_dict = defaultdict(set)
 
 input_test = "/Users/mingleizhao/Git_projects/pharmacy_counting/input/itcont.txt"
 
 with open(input_test,'r') as f:
     inputs = f.readlines()
-
-a = set()
-a.add(1)
-a.add(1)
-1 in a
-
-a = {}
+    
 
 def quick_update(drug_num_cost, subcriber_dict, line):
+    """Return dictionary drug_num_cost and dictionary subscriber_dict of information about all drugs."""
     fields = line.split(",")
     drug_name = fields[-2]
     drug_cost = int(fields[-1].rstrip('\n'))
@@ -37,15 +26,16 @@ for line in inputs[1:]:
     drug_num_cost, subcriber_dict = quick_update(drug_num_cost, sucriber_dict, line)
 
 
-
-#class to store the required fields in output (like a namedtuple)
-class output_tuple():
+class OutputTuple():
+    """A simple attempt to store the required fields in output."""
+    
     def __init__(self, name, cost, count):
+        """Initialize attributes of the OutputTuple class."""
         self.name = name
         self.cost = cost
         self.count = count
 
-tuple_list_unsorted = [output_tuple(k,v['cost'],v['count']) for k,v in drug_num_cost.items()]
+tuple_list_unsorted = [OutputTuple(k,v['cost'],v['count']) for k,v in drug_num_cost.items()]
 
 tuple_list_sorted = sorted(tuple_list_unsorted, key = lambda tuple_: (-tuple_.cost, tuple_.name))
 
